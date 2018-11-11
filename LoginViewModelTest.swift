3273d287cb4ebfdd303703694fb092df727625ba
Damian Kolasiński <32767222+damiankolasinski@users.noreply.github.com>
2018-11-11 22:29:05 +0100

    Create LoginViewModelTest.swift

---
+++ LoginViewModelTest.swift
@@ -0,0 +1,9 @@
+func testDidTapSignIn_givenSuccessfulResponseFromNetwork_shouldPresentApplicationController() {
+        dependencies.networkServiceMock.getUserReturnValue = Observable.just(User.mock)
+        
+        sut.didTapSignInButton()
+        
+        XCTAssertEqual(router.showHudCallsCount, 1)
+        XCTAssertEqual(router.hideHudCallsCount, 1)
+        XCTAssertEqual(router.showApplicationControllerCallsCount, 1)
+    }
